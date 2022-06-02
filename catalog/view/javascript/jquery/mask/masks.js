9666443dca1393ac3fdbf3db1421c58ae463a768
Marcos Franca <marcos-vdm@hotmail.com>
2022-06-01 22:39:53 -0300

    stylesheet slide

--- catalog/view/javascript/jquery/mask/masks.js
+++ catalog/view/javascript/jquery/mask/masks.js
@@ -3,12 +3,12 @@ $(function () {
   $('.cep').mask('00.000-000');
 });
 
-window.onload = function () {
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    $('.owl-item').each(function () {
-      const width = $(this).css('width')
-      const divWidth = parseInt(width) / 2
-      $(this).css('width', divWidth.toString() + 'px')
-    });
-  }
-}
+// window.onload = function () {
+//   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//     $('.owl-item').each(function () {
+//       const width = $(this).css('width')
+//       const divWidth = parseInt(width) / 2
+//       $(this).css('width', divWidth.toString() + 'px')
+//     });
+//   }
+// }
