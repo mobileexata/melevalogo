1bee292f12e91a8280e301c6c38b2d9c2e4bca8d
Marcos Franca <marcos-vdm@hotmail.com>
2022-06-08 19:35:35 -0300

    changes for validation input telephone

--- catalog/view/javascript/jquery/mask/masks.js
+++ catalog/view/javascript/jquery/mask/masks.js
@@ -1,14 +1,10 @@
 $(function () {
   $('input[name="telephone"]').mask('(00) 0000-00000');
   $('.cep').mask('00.000-000');
+  $(document).on('blur', 'input[name="telephone"]', function () {
+    if ($(this).val().replace(/\D/g, '') != "" && $(this).val().replace(/\D/g, '').length < 10) {
+      alert("Telefone inválido!")
+      $(this).val("")
+    }
+  });
 });
-
-// window.onload = function () {
-//   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-//     $('.owl-item').each(function () {
-//       const width = $(this).css('width')
-//       const divWidth = parseInt(width) / 2
-//       $(this).css('width', divWidth.toString() + 'px')
-//     });
-//   }
-// }
